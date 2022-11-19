6aa942572f44db33fe695f9977128ecafa0dc360
ab-rai <ab.rai32768@gmail.com>
2022-11-19 23:40:01 +0530

    Adding Parcel and create-react-app

--- Day1/React/app.js
+++ Day1/React/app.js
@@ -1,3 +1,5 @@
-const helloHeading = React.createElement('h1',{},'Hello Using ReactJS')
+import React from "react"
+import  ReactDOM  from "react-dom/client"
+const helloHeading = React.createElement('h1',{},'Hello Using ReactJS!!')
 const rootElement = ReactDOM.createRoot(document.getElementById('root-id'))
 rootElement.render(helloHeading)
