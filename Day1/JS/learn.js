6178b617e648b9ac59c29b6912b2f74f6ed7116d
ab-rai <ab.rai32768@gmail.com>
2022-11-25 16:58:18 +0530

    Day3 Session

---
+++ Day1/JS/learn.js
@@ -0,0 +1,10 @@
+let name;
+console.log(name)
+name= null
+console.log(name)
+name= 'Abhishek'
+console.log(name)
+name= null
+console.log(name)
+name= undefined
+console.log(name)
--- Day1/React/app.js
+++ Day1/React/app.js
@@ -1,5 +1,26 @@
 import React from "react"
 import  ReactDOM  from "react-dom/client"
-const helloHeading = React.createElement('h1',{},'Hello Using ReactJS!!')
+// const helloHeading = React.createElement('h1',{},'Hello Using ReactJS first time using parcel!!')
+// const HelloHeading = ()=> {
+//     return React.createElement('h1',{},'Hello Using ReactJS first time using parcel and component!!')
+// }
+const numOfMonths = 3.0
+// const title = (
+//     <div id = "title">
+//             <h1 className="heading"> Hello TO bootcamp React </h1>
+//             <h2> From zero to hero </h2>
+//             <h3> Will take only {numOfMonths} month </h3>
+//     </div>
+//     );
+const Title = () =>  (
+    <div id = "title">
+            <h1 className="heading"> Hello TO bootcamp React </h1>
+            <h2> From zero to hero </h2>
+            <h3> Will take only {numOfMonths} month </h3>
+    </div>
+    );
+    
 const rootElement = ReactDOM.createRoot(document.getElementById('root-id'))
-rootElement.render(helloHeading)
+// rootElement.render(<HelloHeading />);
+// rootElement.render(title);
+rootElement.render(<Title/>);
